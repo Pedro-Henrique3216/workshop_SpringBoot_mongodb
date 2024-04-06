ba57897cdf7f3c0b3217e3daf622fa36c0d656df
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-04-06 19:20:44 -0300

    refactor class CommentDTO

--- src/main/java/com/pedrohenrique/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/pedrohenrique/workshopmongo/dto/CommentDTO.java
@@ -1,8 +1,5 @@
 package com.pedrohenrique.workshopmongo.dto;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.Date;
 
 public record CommentDTO(String text, Date date, AuthorDTO author) {
