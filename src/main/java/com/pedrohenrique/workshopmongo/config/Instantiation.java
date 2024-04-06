03b8ffea1df1b8a558bad04f87752506108571fd
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-04-06 16:19:08 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/pedrohenrique/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pedrohenrique/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.pedrohenrique.workshopmongo.config;
 
+import com.pedrohenrique.workshopmongo.domain.Post;
 import com.pedrohenrique.workshopmongo.domain.User;
+import com.pedrohenrique.workshopmongo.repository.PostRepository;
 import com.pedrohenrique.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,8 +18,14 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
 
@@ -24,5 +34,12 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        postRepository.deleteAll();
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei felz hoje!", maria);
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/pedrohenrique/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.pedrohenrique.workshopmongo.repository;
+
+import com.pedrohenrique.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
